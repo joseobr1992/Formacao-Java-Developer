757eefe4c50bb16a666354ab6e0e2d0bb94f878f
joseobr1992 <88032563+joseobr1992@users.noreply.github.com>
2023-07-25 20:37:12 -0300

    Projeto Processo Seletivo
    Projeto Processo Seletivo - Formação Java Developer

--- Controle-de-Candidatos/src/Candidatura/ProcessoSeletivo.java
+++ Controle-de-Candidatos/src/Candidatura/ProcessoSeletivo.java
@@ -1,22 +1,60 @@
 package Candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        /*
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
-        */
-        selecaoCandidatos();
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
 
+    }
+
+    public static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando) {
+                tentativasRealizadas++;
+            }
+
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu) {
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas);
+        } else {
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato +", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+        }
+    }
+
+    //método auxiliar
+    public static boolean atender() {
+        return new Random().nextInt(3)==1;
+    }
+
+    public static void imprimirSelecionados(){
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento ");
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de n " + (indice + 1) + " é " + candidatos[indice]);
+        }
+
+        //forma abreviada de interação com Foreach
+        for (String candidato : candidatos  ) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
 
     public static void selecaoCandidatos(){
         String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
         int candidatosSelecionados = 0;
-        int candidatoAtual = 0;
+        int candidatoAtual = 0; //indice do array
         double salarioBase = 2000.0d;
         while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
             String candidato = candidatos[candidatoAtual];
